5617793ff80be753f2c6a7e324e44f3857e9078b
Isaac Dunn <gandalfthebignose@gmail.com>
2021-01-24 18:51:18 +0000

    Added vector class
    Added vector class to allow for calculations with vectors.

---
+++ FGGC Framework Semester 2/Vector3D.cpp
@@ -0,0 +1,161 @@
+#include "Vector3D.h"
+using namespace DirectX;
+
+vector3d::vector3d()
+{
+	x = 0;
+	y = 0;
+	z = 0;
+}
+
+vector3d::vector3d(float x1, float y1, float z1)
+{
+	x = x1;
+	y = y1;
+	z = z1;
+}
+
+vector3d::vector3d(const vector3d& vec)
+{
+	x = vec.x;
+	y = vec.y;
+	z = vec.z;
+}
+
+//adds vector
+vector3d vector3d::operator+(const vector3d& vec)
+{
+	return vector3d(x + vec.x, y + vec.y, z + vec.z);
+}
+
+//plus equals vector
+vector3d& vector3d::operator+=(const vector3d& vec)
+{
+	x += vec.x;
+	y += vec.y;
+	z += vec.z;
+	return *this;
+}
+
+//minus vector
+vector3d vector3d::operator-(const vector3d& vec)
+{
+	return vector3d(x - vec.x, y - vec.y, z - vec.z);
+}
+
+//minus equals vector
+vector3d& vector3d::operator-=(const vector3d& vec)
+{
+	x -= vec.x;
+	y -= vec.y;
+	z -= vec.z;
+	return *this;
+}
+
+//multiplies vector
+vector3d vector3d::operator*(float value)
+{
+	return vector3d(x * value, y * value, z * value);
+}
+
+//multiple equals vector
+vector3d& vector3d::operator*=(float value)
+{
+	x *= value;
+	y *= value;
+	z *= value;
+	return *this;
+}
+
+//divides vector
+vector3d vector3d::operator/(float value)
+{
+	assert(value != 0);
+	return vector3d(x * value, y * value, z * value);
+}
+
+//divide equals vector
+vector3d& vector3d::operator/=(float value)
+{
+	assert(value != 0);
+	x /= value;
+	y /= value;
+	z /= value;
+	return *this;
+}
+
+//sets vector to another
+vector3d& vector3d::operator=(const vector3d& vec)
+{
+	return vector3d(x = vec.x, y = vec.y, z = vec.z);
+}
+
+//finds dot product between 2 vectors
+float vector3d::dot_product(const vector3d& vec)
+{
+	return (vec.x * x) + (vec.y * y) + (vec.z * z);
+}
+
+//finds cross product between 2 vectors
+vector3d vector3d::cross_product(const vector3d& vec)
+{
+	return vector3d(vec.y*z - vec.z*y,
+					vec.z*x - vec.x*z,
+					vec.x*y - vec.y*x);
+}
+
+//normalizes vector
+vector3d vector3d::normalization()
+{
+	float unitVector = sqrt((x * x) + (y * y) + (z * z));
+
+	x = x / unitVector;
+	y = y / unitVector;
+	z = z / unitVector;
+
+	return vector3d();
+}
+
+//calculate normal from 3 vectors
+void vector3d::ComputeNormal(const vector3d& p0, const vector3d& p1, const vector3d& p2, vector3d& out)
+{
+	vector3d u = { p1.x - p0.x, p1.y - p0.y, p1.z - p0.z };
+	vector3d v = { p2.x - p0.x, p2.y - p0.y, p2.z - p0.z };
+
+	out = { u.y * v.z - u.z * v.y, u.z * v.x - u.x * v.z, u.x * v.y - u.y * v.x };
+}
+
+//square vector
+float vector3d::square(const vector3d& vec)
+{
+	return (sqrt(vec.x), sqrt(vec.y), sqrt(vec.z));
+}
+
+//find distance between vector
+float vector3d::distance(const vector3d& vec)
+{
+	return (vec.x-x, vec.y-y, vec.z-z);
+}
+
+//find magnitude of vector
+float vector3d::magnitude(const vector3d& vec)
+{
+	return (sqrtf(powf(vec.x, 2) + powf(vec.y, 2) + powf(vec.z, 2)));
+}
+
+//finds x of vector
+float vector3d::show_X()
+{
+	return x;
+}
+//finds y of vector
+float vector3d::show_Y()
+{
+	return y;
+}
+//finds z of vector
+float vector3d::show_Z()
+{
+	return z;
+}
+
---
+++ FGGC Framework Semester 2/Vector3D.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include<iostream>
+#include<math.h>
+#include<assert.h>
+
+#include <windows.h>
+#include <d3d11_1.h>
+#include <d3dcompiler.h>
+#include <directxmath.h>
+#include <directxcolors.h>
+#include "resource.h"
+#include <vector>
+
+using namespace std;
+
+class vector3d
+{
+public:
+    float x, y, z;
+
+    //Constructors
+    vector3d();  //constructor
+    vector3d(float x1, float y1, float z1 = 0);  //construct with values.
+    vector3d(const vector3d & vec); //copy constructor
+    
+        //Arithemetic Operators – note use of overloading
+    vector3d    operator+(const vector3d& vec);         //addition
+    vector3d&   operator+=(const vector3d& vec);        //assign new result to vector
+    vector3d    operator-(const vector3d& vec);         //substraction
+    vector3d&   operator-=(const vector3d& vec);        //assign new result to vector
+    vector3d    operator*(float value);             //multiplication
+    vector3d&   operator*=(float value);            //assign new result to vector.
+    vector3d    operator/(float value);             //division
+    vector3d&   operator/=(float value);            //assign new result to vector
+    vector3d&   operator=(const vector3d& vec);
+
+    //Vector operations
+    float dot_product(const vector3d& vec);                 //scalar dot_product
+    vector3d cross_product(const vector3d& vec);            //cross_product
+    vector3d normalization();                                //normalized vector
+    void ComputeNormal(const vector3d& p0, const vector3d& p1, const vector3d& p2, vector3d& out);  
+
+    //Scalar operations
+    float square(const vector3d& vec);   //gives square of the vector
+    float distance(const vector3d& vec); //distance between two vectors
+    float magnitude(const vector3d& vec);  //magnitude of the vector
+
+
+    //Display operations 
+    float show_X(); //return x
+    float show_Y(); //return y
+    float show_Z(); //return z
+};
